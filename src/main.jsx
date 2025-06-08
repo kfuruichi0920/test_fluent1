1597a4672a661775b656ef46d509f1f11f4f43be
kfuruichi0920 <kfuruichi0920.redmine@gmail.com>
2025-06-08 20:49:41 +0900

    ボタンに対して、FluentUIを適用。

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <FluentProvider theme={webLightTheme}>
     <App />
-  </StrictMode>,
-)
+  </FluentProvider>,
+);
